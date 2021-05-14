daac4fe36b379fe2e0e3cd02a220abc88d0112fb
Torstein Wilhelmsen Sandvik <torsteinws@gmail.com>
2021-05-14 14:46:36 +0200

    Create a simple express server to serve a simple html file.

---
+++ node-server/server.js
@@ -0,0 +1,11 @@
+'use strict'
+const path = require('path')
+const express = require('express')
+
+const PORT = 3000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+app.listen(PORT, '', console.log(`Listening on port ${PORT}`))
